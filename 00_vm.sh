3db827b813d802e93b8836f62f919f8ce2650d86
panduola4022 <62542028+panduola4022@users.noreply.github.com>
2025-03-04 05:06:14 +0800

    Update 00_vm.sh

--- 00_vm.sh
+++ 00_vm.sh
@@ -12,7 +12,7 @@ purple() { echo -e "\e[1;35m$1\033[0m"; }
 reading() { read -p "$(red "$1")" "$2"; }
 export LC_ALL=C
 USERNAME=$(whoami)
-# USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
+USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
 MD5_HASH=$(echo -n "$USERNAME" | md5sum | awk '{print $1}')
 export UUID=${UUID:-${MD5_HASH:0:8}-${MD5_HASH:8:4}-4${MD5_HASH:12:3}-$(echo $((RANDOM % 4 + 8)) | head -c 1)${MD5_HASH:15:3}-${MD5_HASH:19:12}}
 export NEZHA_SERVER=${NEZHA_SERVER:-''}     # 哪吒面板地址
@@ -304,12 +304,6 @@ generate_links() {
   sleep 1
   isp=$(curl -s --max-time 2 https://speed.cloudflare.com/meta | awk -F\" '{print $26}' | sed -e 's/ /_/g' || echo "00")
   get_name() { if [ "$HOSTNAME" = "s1.ct8.pl" ]; then SERVER="CT8"; else SERVER=$(echo "$HOSTNAME" | cut -d '.' -f 1); fi; echo "$SERVER"; }
-
-  # 检查 USERNAME, CFIP, CFPORT, UUID 是否已设置
-  if [ -z "${USERNAME}" ] || [ -z "${CFIP}" ] || [ -z "${CFPORT}" ] || [ -z "${UUID}" ] || [ -z "${SUB_TOKEN}" ]; then
-    echo "Error: One or more required variables are not set (USERNAME, CFIP, CFPORT, UUID, SUB_TOKEN)."
-    return 1
-  fi
   NAME=${isp}-$(get_name)-vmess-argo-${USERNAME}
   FILE_PATH="/usr/home/${USERNAME}/domains/${USERNAME}.serv00.net/public_html"
 
@@ -320,15 +314,25 @@ generate_links() {
     return 1
   fi
 
-  # 检查文件是否成功写入
-  if [ ! -f "${FILE_PATH}/${SUB_TOKEN}_vmess.log" ]; then
-    echo "Error: Failed to write to ${FILE_PATH}/${SUB_TOKEN}_vmess.log."
+   # 验证目录权限
+  if [ ! -w "${HOME}/domains" ]; then
+    echo -e "\e[31m错误：缺少目录权限\e[0m" >&2
+    echo -e "请执行以下命令创建目录："
+    echo -e "  mkdir -p ${HOME}/domains/${USERNAME}.serv00.net/public_html"
+    echo -e "  chmod 755 ${HOME}/domains && chmod 755 ${HOME}/domains/${USERNAME}.serv00.net"
     return 1
   fi
   
   cat > ${FILE_PATH}/${SUB_TOKEN}_vmess.log <<EOF
 vmess://$(echo "{ \"v\": \"2\", \"ps\": \"${NAME}\", \"add\": \"${CFIP}\", \"port\": \"${CFPORT}\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"auto\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${argodomain}\", \"path\": \"vmess-argo?ed=2048\", \"tls\": \"tls\", \"sni\": \"${argodomain}\", \"alpn\": \"\" }" | base64 -w0)
 EOF
+
+  # 检查文件是否成功写入
+  if [ ! -f "${FILE_PATH}/${SUB_TOKEN}_vmess.log" ]; then
+    echo "Error: Failed to write to ${FILE_PATH}/${SUB_TOKEN}_vmess.log."
+    return 1
+  fi
+  
   cat ${FILE_PATH}/${SUB_TOKEN}_vmess.log
   green "\n订阅连接: https://${USERNAME}.serv00.net/${SUB_TOKEN}_vmess.log 适用于V2ranN/Nekobox/Karing/小火箭/sterisand/Loon 等\n" 
   rm -rf config.json fake_useragent_0.2.0.json ${WORKDIR}/boot.log ${WORKDIR}/tunnel.json ${WORKDIR}/tunnel.yml 
